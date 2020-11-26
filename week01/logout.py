9da126bf024fb4f82784dad4aea0ea7cf19926a5
lijunjiang2020 <lijunjiang2012@163.com>
2020-11-27 01:10:18 +0800

    作业

---
+++ week01/logout.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+from datetime import datetime
+import logging
+                   )
+
+now_date = datetime.strftime(datetime.now(), "%Y-%m-%d")   
+dirpath = os.getcwd() + "/var/log/python-" + now_date
+logfile = "logout.log"
+
+def logToFile(logfilename):
+    logfile = os.path.join(dirpath, logfilename)
+    
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+    logging.basicConfig(filename=logfile, 
+                    level="DEBUG",
+                    format="%(asctime)s %(name)s %(levelname)s [line: %(lineno)d] %(message)s"
+                        )
+    
+    logging.info("The function logToFile is called")
+
+
+    
+if __name__ == "__main__":
+    logToFile(logfile)
